c16ac0c888809bac7f93df2d2084acee8024cbaf
YKalashnikov <uk2709@gmail.com>
2019-05-31 23:29:05 -0400

    adding mongoose dotenv

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
+
+dotenv.config();
+
+//Conect to Db  
+mongoose.connect(process.env.MONGO_DB,{ useNewUrlParser: true },()=>console.log('Running DB'));
 
 const authRouter = require('./routes/auth');
 
