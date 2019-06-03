3a3a55e101827157af2767a04fa4b1b9340cd2a4
YKalashnikov <uk2709@gmail.com>
2019-06-02 23:33:45 -0400

    emailExist validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,27 @@
+
 const router = require('express').Router();
 const User = require('../models/User.js');
+const Joi = require('@hapi/joi');
+const {registerValidation, loginValidation} = require('/Users/yuriikalashnikov/Desktop/Build A Restful Api With Node.js Express & MongoDB _ Rest Api/validation.js');
 
-router.post('/register', async(req,res) => {
-    const user = new User ({
+
+router.post('/register', async (req, res) => {
+    //Validate the data 
+   const { error } = registerValidation(req.body);
+   if(error) return res.status(400).send(error.details[0].message);
+   //Check a new user if exist 
+   const userExist = await User.findOne({email:req.body.email})
+   if (userExist) return res.status(400).send('Email is already exist')
+    // Create a new user
+    const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
-    })
+    });
     try {
        const savedUser = await user.save();
-       res.send(user);
+       res.send(savedUser);
+      
     } catch(err) {
         res.status(400).send(err);
     };
