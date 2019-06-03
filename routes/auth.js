d04a0485896f0ab93f9f389a3b7c3751120e760b
YKalashnikov <uk2709@gmail.com>
2019-06-03 00:03:23 -0400

    hash password

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,8 @@
 const router = require('express').Router();
 const User = require('../models/User.js');
 const Joi = require('@hapi/joi');
-const {registerValidation, loginValidation} = require('/Users/yuriikalashnikov/Desktop/Build A Restful Api With Node.js Express & MongoDB _ Rest Api/validation.js');
+const {registerValidation, loginValidation} = require('../validation.js');
+const bcrypt = require('bcryptjs');
 
 
 router.post('/register', async (req, res) => {
@@ -12,11 +13,16 @@ router.post('/register', async (req, res) => {
    //Check a new user if exist 
    const userExist = await User.findOne({email:req.body.email})
    if (userExist) return res.status(400).send('Email is already exist')
+
+   //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     // Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try {
        const savedUser = await user.save();
